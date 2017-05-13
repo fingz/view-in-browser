cb46d9d96c60d4ec9ca6439ffdd51f7f3669c3d5
William Lim <manvinedi@gmail.com>
2017-05-13 13:10:07 +0800

    first commit

---
+++ spec/view-in-browser-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ViewInBrowserView from '../lib/view-in-browser-view';
+
+describe('ViewInBrowserView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
